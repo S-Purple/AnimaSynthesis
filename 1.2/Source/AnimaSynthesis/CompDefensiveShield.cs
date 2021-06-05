ab86c5df9507e85901e8a32fe6fc25c4217a4a90
Taranchuk <nfhfyxer@gmail.com>
2021-06-05 13:56:35 +0300

    added new defensive shiel + fixed texture paths

---
+++ 1.2/Source/AnimaSynthesis/CompDefensiveShield.cs
@@ -0,0 +1,71 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AnimaSynthesis
+{
+    public class CompProperties_DefensiveShield : CompProperties_ProjectileInterceptor
+    {
+        public int activeTicks;
+		public CompProperties_DefensiveShield()
+		{
+			compClass = typeof(CompDefensiveShield);
+		}
+	}
+
+	public class CompDefensiveShield : CompProjectileInterceptor
+    {
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            if (!respawningAfterLoad)
+            {
+                Traverse.Create(this).Field("shutDown").SetValue(true);
+            }
+        }
+        public new CompProperties_DefensiveShield Props => this.props as CompProperties_DefensiveShield;
+
+        private int activatedTick;
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            foreach (var g in base.CompGetGizmosExtra())
+            {
+                yield return g;
+            }
+            var command = new Command_Action
+            {
+                defaultLabel = "AS.DefensiveShield".Translate(),
+                defaultDesc = "AS.DefensiveShieldDesc".Translate(),
+                action = delegate
+                {
+                    activatedTick = Find.TickManager.TicksGame;
+                    Traverse.Create(this).Field("shutDown").SetValue(false);
+                }
+            };
+            if (Active)
+            {
+                command.Disable("AS.DefensiveShieldActive".Translate());
+            }
+            yield return command;
+        }
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (this.parent.Spawned && Active && Find.TickManager.TicksGame >= activatedTick + Props.activeTicks)
+            {
+                Traverse.Create(this).Field("shutDown").SetValue(true);
+            }
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref activatedTick, "activatedTick");
+        }
+    }
+}
